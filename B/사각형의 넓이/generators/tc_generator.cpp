d12635a9f83fb8d8b00eeeb84a67df8b247d997e
박정현 <pjh9996@naver.com>
2021-10-28 23:12:42 +0900

    문제 추가: 사각형의 넓이

---
+++ B/사각형의 넓이/generators/tc_generator.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <climits>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <string>
+#include <list>
+#include <ctime>
+#include <complex>
+#include <bitset>
+#include <tuple>
+#include <fstream>
+#include <unistd.h>
+
+#define ff first
+#define ss second
+
+using namespace std;
+using pii=pair<int,int>;
+using ll=long long;
+
+string path="../data/";
+
+string zeropad(int n, int d)
+{
+    string ret=to_string(n);
+    while(ret.length()<d) ret="0"+ret;
+    return ret;
+}
+
+int solution(int a, int b)
+{
+    return a*b;
+}
+
+void generator_random(int datacnt)
+{
+    int data=1;
+    while(data<=datacnt){
+        ofstream osin,osans;
+        osin.open(path+"secret/"+"random"+zeropad(data,2)+".in");
+        osans.open(path+"secret/"+"random"+zeropad(data,2)+".ans");
+        int a=rand()%100+1;
+        int b=rand()%100+1;
+        osin<<a<<" "<<b<<"\n";
+        osans<<solution(a,b);
+        data+=1;
+        osin.close();
+        osans.close();
+    }
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+    generator_random(10);
+    return 0;
+}
---
+++ B/사각형의 넓이/solution/README.md
@@ -0,0 +1,21 @@
+# 솔루션
+
+사각형의 넓이는 가로 x 세로다.
+
+W * H 를 출력한다.
+
+
+
+# 소스코드
+
+```cpp
+#include <iostream>
+using namespace std;
+int main()
+{
+    int w,h;
+    cin>>w>>h;
+    cout<<w*h;
+    return 0;
+}
+```
